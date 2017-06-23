63ee3a50ebb641fc1a0cf3a5ae25604c6a095f37
Sam <skwarte@purdue.edu>
2017-06-23 16:29:01 -0400

    finishing up algorithm

--- binpuzz.cpp
+++ binpuzz.cpp
@@ -45,4 +45,119 @@ int main(int argc, char * argv[])
         }
         cout << "\n" << endl;
     }
+
+    while(1)
+    {
+        for(int i = 0; i < 10; i++)
+        {
+            for(int j = 0; j < 10; j++)
+            {
+                //checks for double 0's and double 1's
+                if(i >= 2)
+                {
+                    if(arr[i-1][j] == 0 && arr[i-2][j]==0)
+                    {
+                        arr[i][j]=1;
+                    }
+                    if(arr[i-1][j] == 1 && arr[i-2][j]==1)
+                    {
+                        arr[i][j]=0;
+                    }
+                }
+                if(i <= 7)
+                {
+                    if(arr[i+1][j] == 0 && arr[i+2][j]==0)
+                    {
+                        arr[i][j]=1;
+                    }
+                    else if(arr[i+1][j] == 1 && arr[i+2][j]==1)
+                    {
+                        arr[i][j]=0;
+                    }
+                }
+                if(j >= 2)
+                {
+                    if(arr[i][j-1] == 0 && arr[i][j-2]==0)
+                    {
+                        arr[i][j]=1;
+                    }
+                    if(arr[i][j-1] == 1 && arr[i][j-2]==1)
+                    {
+                        arr[i][j]=0;
+                    }
+                }
+                if(j <= 7)
+                {
+                    if(arr[i][j+1] == 0 && arr[i][j+2]==0)
+                    {
+                        arr[i][j]=1;
+                    }
+                    else if(arr[i][j+1] == 1 && arr[i][j+2]==1)
+                    {
+                        arr[i][j]=0;
+                    }
+                }
+                
+                //checks for the pattern '0 _ 0' and '1 _ 1'
+                if(i >= 1 && i <= 8)
+                {
+                    if(arr[i-1][j] == 0 && arr[i+1][j]==0)
+                    {
+                        arr[i][j]=1;
+                    }
+                    if(arr[i-1][j] == 1 && arr[i-2][j]==1)
+                    {
+                        arr[i][j]=0;
+                    }
+                }
+                if(i <= 7)
+                {
+                    if(arr[i+1][j] == 0 && arr[i+2][j]==0)
+                    {
+                        arr[i][j]=1;
+                    }
+                    else if(arr[i+1][j] == 1 && arr[i+2][j]==1)
+                    {
+                        arr[i][j]=0;
+                    }
+                }
+                if(j >= 2)
+                {
+                    if(arr[i][j-1] == 0 && arr[i][j-2]==0)
+                    {
+                        arr[i][j]=1;
+                    }
+                    if(arr[i][j-1] == 1 && arr[i][j-2]==1)
+                    {
+                        arr[i][j]=0;
+                    }
+                }
+                if(j <= 7)
+                {
+                    if(arr[i][j+1] == 0 && arr[i][j+2]==0)
+                    {
+                        arr[i][j]=1;
+                    }
+                    else if(arr[i][j+1] == 1 && arr[i][j+2]==1)
+                    {
+                        arr[i][j]=0;
+                    }
+                }
+                
+            }
+        }
+        break;
+    }
+    cout << "\n\n\n\n\n" << endl;
+    for(int i = 0; i < 10; i++)
+    {
+        for(int j = 0; j < 10; j++)
+        {
+            if(arr[i][j] == 5)
+                cout << "_   ";
+            else
+                cout << arr[i][j] << "   ";
+        }
+        cout << "\n" << endl;
+    }
 }
